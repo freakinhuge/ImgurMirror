2f38c10d1fe83c9b65ac148931acb16f47fa9969
freakinhuge <freakinhuge@gmail.com>
2013-02-05 19:59:14 -0600

    mirror for qkme

--- index.php
+++ index.php
@@ -3,23 +3,23 @@
 <center>
 <?php
 //CACHED VERSION
-$imgur = $_SERVER['QUERY_STRING'];
+$qkme = $_SERVER['QUERY_STRING'];
 	//Grab the image url from the query string
 $imgWidth = '50%';
 	//Sets the global width of images
 $imgBorder = '2';
 	//Sets the global image border size
-if(preg_match('/^a\/([a-z0-9]{5,7})/i', $imgur)) {
+if(preg_match('/^a\/([a-z0-9]{5,7})/i', $qkme)) {
 	//Check if url is that of an albumn
-	$albumn = file_get_contents("http://imgur.com/".$imgur);
+	$albumn = file_get_contents("http://qkme.me/".$qkme);
 		//Download the image albumn html into a variable
-	preg_match_all('/http:\/\/i\.imgur\.com\/([a-z0-9]{5,7})\.(png|jpg|gif|jpeg)/i', $albumn, $out, PREG_PATTERN_ORDER);
+	preg_match_all('/http:\/\/i\.qkme\.me\/([a-z0-9]{5,7})\.(png|jpg|gif|jpeg)/i', $albumn, $out, PREG_PATTERN_ORDER);
 	$matches = preg_grep('/([a-z0-9]{5,7})\.(png|jpg|gif|jpeg)/i', $out[0]);
 		//Find all images in the albumn and store their filenames in an array
 		//Set the whole match array to variable
 	$matches = array_unique($matches);
 	foreach($matches as $value) {
-		$fname="./cache/".str_replace("http://i.imgur.com/", "", $value);
+		$fname="./cache/".str_replace("http://i.qkme.me/", "", $value);
 			//Set the cached filename
 		if(file_exists($fname)){	
 			//Check if the file is already cached
@@ -29,10 +29,10 @@ if(preg_match('/^a\/([a-z0-9]{5,7})/i', $imgur)) {
 		else {
 			$con = stream_context_create(array('http'=>array('timeout'=>15)));
 				//Prevent long running timed out downloads, but long enough to get large files
-			$image = @file_get_contents('http://i.imgur.com/'.str_replace("http://i.imgur.com/", "", $value),0,$con);
+			$image = @file_get_contents('http://i.qkme.me/'.str_replace("http://i.qkme.me/", "", $value),0,$con);
 				//Download the image into a variable
 			if(!$image) {
-				echo "Cannot retrieve imgur file";
+				echo "Cannot retrieve qkme file";
 					//If the download failed, tell us
 				die();
 					//And exit
@@ -40,15 +40,15 @@ if(preg_match('/^a\/([a-z0-9]{5,7})/i', $imgur)) {
 			else {
 				file_put_contents($fname, $image);
 					//Save the image to cache
-				echo "<img width=$imgWidth border=$imgBorder src=\"".$fname."\">" . "<br><br>";
+				echo "<img width=$imgWidth border=$imgBorder  src=\"".$fname."\">" . "<br><br>";
 					//Add the cached file to the html
 			}
 		}
 	}
 }
-else if(preg_match('/^([a-z0-9]{5,7})\.(png|jpg|gif|jpeg)/i', $imgur)) {
-	//Make sure only valid imgur filenames are allowed, 5-7 characters, and png, gif, jpg or jpeg extensions
-	$fname="./cache/".$imgur;
+else if(preg_match('/^([a-z0-9]{5,7})\.(png|jpg|gif|jpeg)/i', $qkme)) {
+	//Make sure only valid qkme filenames are allowed, 5-7 characters, and png, gif, jpg or jpeg extensions
+	$fname="./cache/".$qkme;
 		//Set the cached filename
 	if(file_exists($fname)) {	
 		//Check if the file is already cached
@@ -58,10 +58,10 @@ else if(preg_match('/^([a-z0-9]{5,7})\.(png|jpg|gif|jpeg)/i', $imgur)) {
 	else {
 		$con = stream_context_create(array('http'=>array('timeout'=>15)));
 			//Prevent long running timed out downloads, but long enough to get large files
-		$image = @file_get_contents('http://i.imgur.com/'.$imgur,0,$con);
+		$image = @file_get_contents('http://i.qkme.me/'.$qkme,0,$con);
 			//Download the image into a variable
 		if(!$image) {
-			echo "Cannot retrieve imgur file";
+			echo "Cannot retrieve qkme file";
 				//If the download failed, tell us
 			die();
 				//And exit
@@ -74,9 +74,9 @@ else if(preg_match('/^([a-z0-9]{5,7})\.(png|jpg|gif|jpeg)/i', $imgur)) {
 		}	
 	}	
 }
-else if(preg_match('/^([a-z0-9]{5,7})/i', $imgur)) {
-	//Make sure only valid imgur filenames are allowed, 5-7 characters, and png, gif, jpg or jpeg extensions
-	$fname="./cache/".$imgur.".jpg";
+else if(preg_match('/^([a-z0-9]{5,7})/i', $qkme)) {
+	//Make sure only valid qkme filenames are allowed, 5-7 characters, and png, gif, jpg or jpeg extensions
+	$fname="./cache/".$qkme.".jpg";
 		//Set the cached filename
 	if(file_exists($fname)) {	
 		//Check if the file is already cached
@@ -86,10 +86,10 @@ else if(preg_match('/^([a-z0-9]{5,7})/i', $imgur)) {
 	else {
 		$con = stream_context_create(array('http'=>array('timeout'=>15)));
 			//Prevent long running timed out downloads, but long enough to get large files
-		$image = @file_get_contents('http://i.imgur.com/'.$imgur.".jpg",0,$con);
+		$image = @file_get_contents('http://i.qkme.me/'.$qkme.".jpg",0,$con);
 			//Download the image into a variable
 		if(!$image) {
-			echo "Cannot retrieve imgur file";
+			echo "Cannot retrieve qkme file";
 				//If the download failed, tell us
 			die();
 				//And exit
@@ -103,7 +103,7 @@ else if(preg_match('/^([a-z0-9]{5,7})/i', $imgur)) {
 	}	
 }
 else {
-	die("Not a valid imgur image");
+	die("Not a valid qkme image");
 		//Exit if it's not valid
 }
 ?>
